3650e436ed6c2830186aa6175d6ce3006e4d9730
yoanarobeva <yoana_robeva@yahoo.com>
2023-01-19 20:23:03 +0200

    configurated project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const config = require('./config/config');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+});
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
