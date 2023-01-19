a77b3f5e1f3e5d799d280bd117a6faceb8df34c3
yoanarobeva <yoana_robeva@yahoo.com>
2023-01-19 20:56:30 +0200

    render home, about and create

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+
 const config = require('./config/config');
+const setupViewEngine = require('./config/viewEngine');
+
 const app = express();
+setupViewEngine(app);
+
+app.use(express.static('src/static'));
 
 app.get('/', (req, res) => {
-    res.send('Home page')
+    res.render('index')
 });
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
