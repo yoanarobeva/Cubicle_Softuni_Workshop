e7021ad930c372f59a78120b187671412cf801c3
yoanarobeva <yoana_robeva@yahoo.com>
2023-02-04 15:46:35 +0200

    login and register page added

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 
 router.use('/cube', cubeController);
 router.use('/accessory', accessoryController);
+router.use('/auth', authController);
 
 router.get('*/404', homeController.getErrorPage);
 
