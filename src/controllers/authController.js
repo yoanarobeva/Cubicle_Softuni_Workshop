30ea72459c2a5e2a2a3b9233ccda9a786a07dbf6
yoanarobeva <yoana_robeva@yahoo.com>
2023-02-04 16:27:42 +0200

    Register working

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,26 +1,37 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
+    await authService.login(username, password);
 });
 
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     const { username, password, repeatPassword } = req.body;
 
     if (password !== repeatPassword) {
         alert('Passwords dont match!');
-        return res.status(404).end();
+        return res.redirect('/404');
     }
 
-    
+    const existingUser = await authService.getUserByUsername(username);
+
+    if (existingUser) {
+        return res.redirect('/404');
+    }
+
+    const user = await authService.register(username, password);
+
+    res.redirect('/auth/login');
 });
 
 module.exports = router;
--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,32 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        minLength: 3,
     }, 
-    
+    password: {
+        type: String,
+        required: true,
+        minLength: [4, 'Password must be at least 4 characters!']
+    }
 });
+
+userSchema.method('validatePassword', async function(password) {
+    return bcrypt.compare(password, this.password);
+})
+
+userSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+
+            next();
+        });
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
